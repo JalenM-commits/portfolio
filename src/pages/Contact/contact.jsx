07f752cb735e1477255d6bb2f8f775a5c7a119d9
JalenM-commits <jmendon8@my.centennialcollege.ca>
2024-09-21 13:59:01 -0400

    added contact form functionality

--- src/pages/Contact/contact.jsx
+++ src/pages/Contact/contact.jsx
@@ -6,17 +6,34 @@ Jalen Mendonca - 301098614
 portfolio_jm
 */
 
-import React from "react";
-import "./contact.css"
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser';
+import "./contact.css";
 
 const Contact = () => {
+    const form = useRef;
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_1bsnlwa', 'template_du8sxpk', form.current, {
+            publicKey: '616Np2IRI71ePqssj',
+        })
+          .then(
+            () => {
+            console.log('SUCCESS!');
+            },
+                (error) => {
+                console.log('FAILED...', error.text);
+                },
+            );
+        };
     return (
         <div className="contactPage">
             <h1 className="contactPageTitle">Contact Me</h1>
             <span className="contactDesc">Please fill out this form to discuss any work opportunities.</span>
-            <form className="contactForm">
-                <input type="text" className="name" placeholder="Your Name"/>
-                <input type="email" className="email" placeholder="Your Email"/>
+            <form className="contactForm" ref={form} onSubmit={sendEmail}>
+                <input type="text" className="name" placeholder="Your Name" name="your_name" />
+                <input type="email" className="email" placeholder="Your Email" name="your_email" />
                 <textarea className="msg" name="message" rows={5} placeholder="Your Message"></textarea>
                 <button type="submit" value='send' className="submitBtn">Submit</button>
             </form>
